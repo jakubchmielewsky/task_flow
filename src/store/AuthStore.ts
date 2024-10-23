ccbeb7748b0fff88cfc54c1e3f06d4409f0a9039
Jakub Chmielwski <kuba91821@gmail.com>
2024-10-23 20:44:15 +0200

    added authStore

---
+++ src/store/AuthStore.ts
@@ -0,0 +1,54 @@
+import {create} from 'zustand'
+import {onAuthStateChanged, signInWithEmailAndPassword, signOut, User} from 'firebase/auth'
+import {auth} from '../firebase/firebaseConfig'
+import { FirebaseError } from 'firebase/app';
+
+interface AuthState{
+    user: User | null;
+    loading: boolean;
+    error: string | null;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    setUser: (user: User | null) => void;
+}
+
+export const useAuthStore = create<AuthState>((set)=>({
+    user:null,
+    loading: true,
+    error: null,
+
+    login: async (email, password) => {
+        set({loading: true, error: null});
+        try{
+            await signInWithEmailAndPassword(auth,email,password);
+            set({loading: false});
+        }catch (error) {
+            if (error instanceof FirebaseError) {
+              set({ error: error.message, loading: false });
+            } else {
+              set({ error: "Unexpected error", loading: false });
+            }
+          }
+    },
+
+    logout: async () => {
+        set({ loading: true, error: null });
+        try {
+          await signOut(auth);
+          set({ user: null, loading: false });
+        } catch (error) {
+            if (error instanceof FirebaseError) {
+              set({ error: error.message, loading: false });
+            } else {
+              set({ error: "Unexpected error", loading: false });
+            }
+          }
+      },
+
+      setUser: (user) => set({ user, loading: false }),
+
+}));
+
+onAuthStateChanged(auth, (user) => {
+    useAuthStore.getState().setUser(user);
+});
